1c2d0897fc8382a0e45c6c2f089a83a30b616381
SangWookNa <sktkddnro0o0@gmail.com>
2019-05-09 23:50:32 +0900

    20190509_react-router ADD

--- server/main.js
+++ server/main.js
@@ -14,14 +14,14 @@ app.use('/api', api);
 
 if (process.env.NODE_ENV == 'development') {
     app.use(express.static(path.join(__dirname, '..', 'public/')));
-    app.get('/*', function (req, res) {
-        res.sendFile(path.join(__dirname, './../public', 'index.html'));
-      });
+    // app.get('/*', function (req, res) {
+    //     res.sendFile(path.join(__dirname, './../public', 'index.html'));
+    //   });
 }else{
     app.use(express.static(path.join(__dirname, '..', 'build/')));
-    app.get('/*', function (req, res) {
-        res.sendFile(path.join(__dirname, './../build', 'index.html'));
-      });
+    // app.get('/*', function (req, res) {
+    //     res.sendFile(path.join(__dirname, './../build', 'index.html'));
+    //   });
 }
 
 app.use(morgan('dev'));
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 
-class Login extends Component {
+class Header extends Component {
 
   render() {
 
     return (
       <div>
-        Login
+        Header
       </div>
     );
   }
 }
 
-export default Login;
+export default Header;
