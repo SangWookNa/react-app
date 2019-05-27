570ad23d071cdd5005d10c56cdcaf21e32f75cce
SangWookNa <sktkddnro0o0@gmail.com>
2019-05-27 23:44:52 +0900

    20190527

--- src/containers/ReactFileUpload.js
+++ src/containers/ReactFileUpload.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ImageUploader from 'react-images-upload';
 import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { post } from 'axios';
 
 class ReactFileUpload extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { pictures: [] };
+        this.state = { pictures: [],
+                        loadingFlag: false,
+                        loadingValue: 0
+         
+        };
         this.onDrop = this.onDrop.bind(this);
     }
     
@@ -19,11 +24,11 @@ class ReactFileUpload extends React.Component {
     }
 
     handleUpload = () => {
-        const url = 'http://localhost:3000/api/image';
+        const url = 'http://192.168.0.6:3000/api/image';
         const formData = new FormData();
 
         let file = this.state.pictures;
-        formData.append('file', file);
+
         formData.append('test', 'test');
         for(let i=0; i < file.length; i++ ){
             formData.append('file', file[i]);
@@ -31,13 +36,22 @@ class ReactFileUpload extends React.Component {
         const config = {
             headers: {
                 'content-type': 'multipart/form-data'
+            },
+            onUploadProgress: progressEvent => {
+                //alert('test: '+Math.round( (progressEvent.loaded * 100) / progressEvent.total ));
+                this.setState({
+                    loadingFlag: true,
+                    loadingValue: Math.round( (progressEvent.loaded * 100) / progressEvent.total )
+                })
             }
         }
         return post(url, formData, config).then((result) => {
             
             if(result.data.success === true){
                  this.setState({
-                     pictures : []
+                     pictures : [],
+                     loadingFlag: false,
+                     loadingValue:0
                  });
                  formData.delete('file');
                 
@@ -52,6 +66,8 @@ class ReactFileUpload extends React.Component {
     }
 
     render() {
+        
+        const loading = (<CircularProgress variant="static" value={this.state.loadingValue} />);
         return (
             <div>
                 <ImageUploader
@@ -59,12 +75,14 @@ class ReactFileUpload extends React.Component {
                     withIcon={false}
                     buttonText='upload'
                     onChange={this.onDrop}
-                    imgExtension={['.jpg', '.gif', '.png', '.gif']}
+                    imgExtension={['.jpg', '.gif', '.png', '.gif', '.jpeg']}
                     maxFileSize={5242880}
                     withPreview = {true}
                 />
                 <Button onClick={this.handleUpload}>Upload</Button>
+                {this.state.loadingFlag === true ? loading : undefined}
             </div>
+            
         );
     }
 }
